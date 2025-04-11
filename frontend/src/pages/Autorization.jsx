f0a2a3dba88be6b80182ff6f0fefb2dc090a80ea
Ekaterina Volkova <katerinavolkova2609@gmail.com>
2025-04-11 16:28:25 +0300

    add validation

--- frontend/src/pages/Autorization.jsx
+++ frontend/src/pages/Autorization.jsx
@@ -1,4 +1,6 @@
 import avatar from '../assets/avatar.jpg';
+import * as Yup from 'yup';
+import FormComponent from './components/form.jsx';
 
 const Autorization = () => {
   return (
@@ -15,42 +17,7 @@ const Autorization = () => {
                     alt="Войти"
                   />
                 </div>
-                <form class="col-12 col-md-6 mt-3 mt-md-0">
-                  <h1 class="text-center mb-4">Войти</h1>
-                  <div class="form-floating mb-3">
-                    <input
-                      name="username"
-                      autocomplete="username"
-                      required=""
-                      placeholder="Ваш ник"
-                      id="username"
-                      class="form-control"
-                      value=""
-                    />
-                    <label for="username">Ваш ник</label>
-                  </div>
-                  <div class="form-floating mb-4">
-                    <input
-                      name="password"
-                      autocomplete="current-password"
-                      required=""
-                      placeholder="Пароль"
-                      type="password"
-                      id="password"
-                      class="form-control"
-                      value=""
-                    />
-                    <label class="form-label" for="password">
-                      Пароль
-                    </label>
-                  </div>
-                  <button
-                    type="submit"
-                    class="w-100 mb-3 btn btn-outline-primary"
-                  >
-                    Войти
-                  </button>
-                </form>
+                <FormComponent />
               </div>
               <div class="card-footer p-4">
                 <div class="text-center">
---
+++ frontend/src/pages/components/form.jsx
@@ -0,0 +1,76 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const FormComponent = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      console.log('submit', values);
+    },
+    validationSchema: Yup.object().shape({
+      username: Yup.string().required('Please enter login'),
+      password: Yup.string().required('Please enter password'),
+    }),
+  });
+  return (
+    <form
+      className="col-12 col-md-6 mt-3 mt-md-0"
+      onSubmit={formik.handleSubmit}
+    >
+      <h1 className="text-center mb-4">Войти</h1>
+      <div className="form-floating mb-3">
+        <input
+          name="username"
+          autocomplete="username"
+          required=""
+          placeholder="Ваш ник"
+          id="username"
+          className={`form-control  ${
+            formik.errors.username && formik.touched.username
+              ? 'is-invalid'
+              : ''
+          }`}
+          value={formik.values.username}
+          onChange={formik.handleChange}
+        />
+        {formik.errors.username && formik.touched.username ? (
+          <div className="text-danger">{formik.errors.username}</div>
+        ) : null}
+        <label htmlFor="username" className="form-label">
+          Ваш ник
+        </label>
+      </div>
+      <div className="form-floating mb-4">
+        <input
+          name="password"
+          autocomplete="current-password"
+          required=""
+          placeholder="Пароль"
+          type="password"
+          id="password"
+          className={`form-control  ${
+            formik.errors.username && formik.touched.username
+              ? 'is-invalid'
+              : ''
+          }`}
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        {formik.errors.password && formik.touched.password ? (
+          <div className="text-danger">{formik.errors.password}</div>
+        ) : null}
+        <label className="form-label" htmlFor="password">
+          Пароль
+        </label>
+      </div>
+      <button type="submit" className="w-100 mb-3 btn btn-outline-primary">
+        Войти
+      </button>
+    </form>
+  );
+};
+
+export default FormComponent;
