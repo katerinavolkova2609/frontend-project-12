6bf168a1771e121ad60e4ab1438b145a24c5b6b9
Ekaterina Volkova <katerinavolkova2609@gmail.com>
2025-04-17 11:44:11 +0300

    fix autorization page

--- frontend/src/pages/Autorization.jsx
+++ frontend/src/pages/Autorization.jsx
@@ -3,26 +3,27 @@ import FormComponent from './components/form.jsx';
 
 const Autorization = () => {
   return (
-    <div>
-      <div class="container-fluid h-100">
-        <div class="row justify-content-center align-content-center h-100">
-          <div class="col-12 col-md-8 col-xxl-6">
-            <div class="card shadow-sm">
-              <div class="card-body row p-5">
-                <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                  <img
-                    src={avatar}
-                    class="rounded-circle"
-                    alt="Войти"
-                  />
+    <div className="d-flex flex-column vh-100" id="chat">
+      <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            Hexlet Chat
+          </a>
+        </div>
+      </nav>
+      <div className="container-fluid flex-grow-1 d-flex justify-content-center align-items-center">
+        <div className="row justify-content-center w-100">
+          <div className="col-12 col-md-8 col-xxl-6">
+            <div className="card shadow-sm">
+              <div className="card-body row p-5">
+                <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                  <img src={avatar} alt="Войти" className="rounded-circle" />
                 </div>
                 <FormComponent />
               </div>
-              <div class="card-footer p-4">
-                <div class="text-center">
-                  <span>Нет аккаунта?</span>
-                  <a href="/signup">Регистрация</a>
-                </div>
+              <div className="card-footer p-4 text-center">
+                <span>Нет аккаунта? </span>
+                <a href="/signup">Регистрация</a>
               </div>
             </div>
           </div>
