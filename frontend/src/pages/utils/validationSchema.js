9b02f93dd47f9686475236ecdb7884210c5ff3ba
Ekaterina Volkova <katerinavolkova2609@gmail.com>
2025-06-16 15:04:48 +0300

    fix bugs

--- frontend/src/pages/utils/validationSchema.js
+++ frontend/src/pages/utils/validationSchema.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup'
 const getSchema = (namesOfChannels, t) => Yup.object().shape({
   channel: Yup.string()
     .min(3, t('validation.min_max'))
+    .max(20, t('validation.min_max'))
     .notOneOf(namesOfChannels, t('validation.uniqChannelName'))
     .required(t('validation.requiredChannelName')),
 })
